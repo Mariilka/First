cff2f9bff37836d50dbed9b410fff80e755c01ea
Marina <aminovamarina2006@gmail.com>
2026-03-03 02:06:59 +0800

    типо

--- Two/Program.cs
+++ Two/Program.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace Two
 {
-    internal class Program
+    public static class Logic
+    { 
+        //НАЧАЛО логики
+            for (int i = 1; i<sequence.Length; i++)
+            {
+                // Проверяем, поменялся ли знак между текущим и предыдущим элементом
+                if ((sequence[i - 1] > 0 && sequence[i] < 0) ||
+                    (sequence[i - 1] < 0 && sequence[i] > 0))
+                {
+                    signChanges++;
+                }
+        }//КОНЕЦ логики
+    }
+        public class Program
     {
         static void Main(string[] args)
         {
@@ -16,21 +29,11 @@ namespace Two
             int signChanges = 0;
             //КОНЕЦ работы с данными
 
-            //НАЧАЛО логики
-            for (int i = 1; i < sequence.Length; i++)
-            {
-                // Проверяем, поменялся ли знак между текущим и предыдущим элементом
-                if ((sequence[i - 1] > 0 && sequence[i] < 0) ||
-                    (sequence[i - 1] < 0 && sequence[i] > 0))
-                {
-                    signChanges++;
-                }
-            }//КОНЕЦ логики
+            
 
             //НАЧАЛО работы с консолью
             Console.WriteLine($"В последовательности [{string.Join(", ", sequence)}]");
             Console.WriteLine($"Знак меняется {signChanges} раз(а)");
             //КОНЕЦ работы с консолью
         }
-    }
 }
