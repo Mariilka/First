4cceafbfc29bc9f8ed84d5ae112ea8f32c0e620b
Marina <aminovamarina2006@gmail.com>
2026-03-03 00:44:54 +0800

    типо декомпозиция

--- First/Program.cs
+++ First/Program.cs
@@ -4,39 +4,61 @@ using System;
 
 namespace First
 {
-    internal class Program
+    static class Logic
+    {
+        public static bool IsThreeDigitNumber(int number)
+        {
+            return number > 99 && number < 1000;
+        }
+
+        public static int FindMaxDigit(int number)
+        {
+            int a = number / 100;
+            number %= 100;
+            int b = number / 10;
+            number %= 10;
+            int c = number;
+            int max;
+
+            if (a >= b && a >= c)
+            {
+                max = a;
+            }
+            else if (b >= a && b >= c)
+            {
+                max = b;
+            }
+            else
+            {
+                max = c;
+            }
+
+            return max;
+        }
+
+        public static void Result(int max)
+        {
+            Console.WriteLine($"Максимальное число: {max}");
+        }
+    }
+
+    class Program
     {
         static void Main(string[] args)
         {
-                Console.WriteLine("Введите трёхзначное число");
+            Console.WriteLine("Введите трёхзначное число");
 
-                int c = Convert.ToInt32(Console.ReadLine());
-                if (c > 99 && c < 1000)
-                {
-                    int a = c / 100;
-                    c %= 100;
-                    int b = c / 10;
-                    c %= 10;
-                    int max;
-                    if (a >= b && a >= c)
-                    {
-                        max = a;
-                    }
-                    else if (b >= a && b >= c)
-                    {
-                        max = b;
-                    }
-                    else
-                    {
-                        max = c;
-                    }
+            int c = Convert.ToInt32(Console.ReadLine());
 
-                    Console.WriteLine($"Максимальное число: {max}");
-
-
-                }
-                else Console.WriteLine("Неверный ввод. Введите трёхзначное число");
-            
+            if (Logic.IsThreeDigitNumber(c))
+            {
+                int maxDigit = Logic.FindMaxDigit(c);
+                Logic.Result(maxDigit);
+            }
+            else
+            {
+                Console.WriteLine("Неверный ввод. Введите трёхзначное число");
+            }
         }
     }
 }
