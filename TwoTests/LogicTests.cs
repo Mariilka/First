6e4ef8d8ce239e4e2148f9b881952d07f26fc573
Marina <aminovamarina2006@gmail.com>
2026-03-03 02:17:14 +0800

    оригинальность преследует меня

---
+++ TwoTests/LogicTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Two;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Two.Tests
+{
+    [TestClass()]
+    public class LogicTests
+    {
+        [TestMethod()]
+        public void CountSignChangesTest()
+        {
+
+            int[] sequence = { 1, 2, 3, 4, 5 };
+            int expected = 0;
+
+            int result = Logic.CountSignChanges(sequence);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void CountSignChangesTest1()
+        {
+            int[] sequence = { 1, -1, 2, -2, 3, -3 };
+            int expected = 5; 
+
+            int result = Logic.CountSignChanges(sequence);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
