4a52b7f24a872ad4447eeea02782e574a0d3aab8
Marina <aminovamarina2006@gmail.com>
2026-03-03 02:02:06 +0800

    быстренько написала 2 код

---
+++ Two/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Two
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sequence = { 10, -4, 12, 56, -4 };
+
+            int signChanges = 0;
+            for (int i = 1; i < sequence.Length; i++)
+            {
+                // Проверяем, поменялся ли знак между текущим и предыдущим элементом
+                if ((sequence[i - 1] > 0 && sequence[i] < 0) ||
+                    (sequence[i - 1] < 0 && sequence[i] > 0))
+                {
+                    signChanges++;
+                }
+            }
+
+            Console.WriteLine($"В последовательности [{string.Join(", ", sequence)}]");
+            Console.WriteLine($"Знак меняется {signChanges} раз(а)");
+
+        }
+    }
+}
